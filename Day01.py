d6711a34bf8aaf82fac0293648ee96217e9922b0
shahrah <dmoooo3_26@hotmail.com>
2019-08-28 23:20:59 +0300

    git commit -m" day01.py"

--- Day01.py
+++ Day01.py
@@ -1,1 +1,114 @@
 print("Hello, World!")
+print("five is greater than two")
+#this is a commit
+#written in
+#more than just one file
+print("Hello, World!")
+"""
+this is a commit
+written in
+more than just one file
+"""
+print("Hello, World!")
+
+x = 5
+y ="john"
+print(x)
+print(y)
+
+x = "is of type int"
+x = "sally" # x = is not of type str
+print(x)
+x, y, z = "orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+x = y = z ="orange"
+print(x)
+print(y)
+print(z)
+x = "awesome"
+print("python is " + x)
+x = " python "
+y = "is great"
+z = x + y
+print(z)
+x = 20
+y = 30
+z = x + y
+print(z)
+x = 5
+z = 3
+y = x + z
+print(y)
+x = 5
+y = "jhon"
+print (x + y)
+x, y, z = "orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+x = y = z ="orange"
+print(x)
+print(y)
+print(z)
+x = "awesome"
+print("python is " + x)
+x = " python "
+y = "is great"
+z = x + y
+print(z)
+x = 20
+y = 30
+z = x + y
+print(z)
+x = 5
+z = 3
+y = x + z
+print(y)
+x = 1   #int
+y = 2.8   #float
+z = 1j   #comlex
+print(type(x))
+print(type(y))
+print(type(z))
+x = 1
+y = 45245147555522
+z = -5484132554
+print(type(x))
+print(type(y))
+print(type(z))
+x = 2.66
+y = 52.36
+z = 5.256
+print(type(x))
+print(type(y))
+print(type(z))
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+x = 5j
+y = +5j
+z = -7j
+print(type(x))
+print(type(y))
+print(type(z))
+x = 5
+y = 2.3
+z = 1j
+a = float(x)
+b = int(y)
+c = complex(x)
+print(type(a))
+print(type(b))
+print(type(c))
+import random
+print(random.randrange(1,10))
+x = "apple"
+y = "orange"
+z = "limon"
+basket = x + y + z
+print(basket)
