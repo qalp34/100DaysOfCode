5352a8ef7764df13c1064a3fa48430598dbd3f4a
shahrah <dmoooo3_26@hotmail.com>
2019-08-30 21:53:44 +0300

    week02 completed

---
+++ week02/Day11.py
@@ -0,0 +1,15 @@
+x = 5
+print(x > 3 or x < 4)
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is not z)
+print(x is not y)
+print(x != y)
+
+x = ("apple","banana")
+print("banana" in x)
+
+
---
+++ week02/Day12.py
@@ -0,0 +1,12 @@
+a = "please,I want {} sandwishes and {} donutes"
+print(a.replace("I","We"))
+
+
+sandwishes = 5
+donutes = 7
+myorder = "please,I want {} sandwishes and {} donutes" 
+print(myorder.format(sandwishes,donutes))
+
+
+a = "please,I want {} sandwishes and {} donutes"
+print(a.replace("a","A"))
